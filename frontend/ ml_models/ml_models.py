a034f908074cf764ba54bc6d088d16ad87b37a2f
Rohitharitash45 <rohit20050928@gmail.com>
2026-05-12 13:52:27 +0530

    Delete frontend/ ml_models/ml_models.py

--- frontend/ ml_models/ml_models.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-print("📦 Loading trained model...")
-
-model = joblib.load("phishing_model.pkl")
-feature_columns = joblib.load("feature_columns.pkl")
-
-print("✅ Model loaded successfully")
